b37ca9fafbd811f55c08808a3a9a355b71897229
chezynick <chezynickannie@yahoo.co.uk>
2020-11-19 11:29:38 +0000

    added aboutus

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+
+//import pages
+import AboutUs from './pages/AboutUs'
+
 function App() {
   return (
     <div className="App">
-     Hello world
+    <AboutUs />
     </div>
   );
 }
